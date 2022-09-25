1bfc1a5cb11ff2bdb8fa9033966ea5b80d4d6bea
JackNeedFocus <Jack.z.deng@outlook.com>
2022-09-25 10:40:33 +0930

    GET client parsing XML done.

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,5 +1,6 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,41 +8,46 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 
 import javax.print.DocFlavor.URL;
 
 public class ClientHandler implements Runnable {
     private Socket client;
-    private OutputStream out;
-    public final static String FILE_PATH_NAME = "./ATOMFeed.txt";
+    private DataOutputStream out;
+    public final static String FILE_PATH_NAME = "./ATOMFeed.xml";
 
     public ClientHandler(Socket clientSocket) throws IOException {
         this.client = clientSocket;
-        out = client.getOutputStream();
+        out = new DataOutputStream(client.getOutputStream());
     }
 
     @Override
     public void run() {
-        FileInputStream fis = null; 
+        FileInputStream fis = null;
         BufferedInputStream bis = null;
-        
+
         try {
             File file = new File(FILE_PATH_NAME);
-            byte[] myByteArray = new byte[(int) file.length()];
-
+            byte[] XMLByte = new byte[(int) file.length()];
 
             // turn the file input stream into buffered input stream
             fis = new FileInputStream(file);
             bis = new BufferedInputStream(fis);
 
             // read the file content into myByteArray
-            bis.read(myByteArray, 0, myByteArray.length);
-            System.out.println("Sending " + FILE_PATH_NAME.substring(2) + "(" + myByteArray.length + " bytes)");
-            
-            // output the myByteArray to the client
-            out.write(myByteArray, 0, myByteArray.length);
-            out.flush();
+            bis.read(XMLByte, 0, XMLByte.length);
+            System.out.println("Sending " + FILE_PATH_NAME.substring(2) + "(" + XMLByte.length + " bytes)");
+
+            // write the GET response
+            String headerFirstLine = "HTTP/1.1 200 OK";
+            byte[] headerFirstLineByte = headerFirstLine.getBytes(Charset.forName("UTF-8"));
+            out.writeInt(headerFirstLineByte.length);
+            out.write(headerFirstLineByte);
+
+            out.writeInt(XMLByte.length);
+            out.write(XMLByte);
 
             System.out.println("Done");
         } catch (IOException e) {
--- GETClient.java
+++ GETClient.java
@@ -1,11 +1,17 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.charset.Charset;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.UUID;
 
 /**
  * contentServer
@@ -16,11 +22,16 @@ public class GETClient {
 
     private static String SERVER_IP;
     private static int SERVER_PORT;
+    private static Deque<Feed> feedQueue = new LinkedList<Feed>();
+    private static UUID uuid;
 
     public static void main(String[] args) throws IOException {
 
+        // generate an unique id for this client
+        uuid = UUID.randomUUID();
+
         // receive user input
-        String URL = args[0]; 
+        String URL = args[0];
 
         // get the domain and port
         String[] domainPort = URL.split(":", 2);
@@ -31,7 +42,6 @@ public class GETClient {
 
         // sending the get request
         DataOutputStream dataOutputStream = new DataOutputStream(server.getOutputStream());
-
 
         // write the GET header
         String headerFirstLine = "GET /getFeed HTTP/1.1";
@@ -49,11 +59,49 @@ public class GETClient {
         dataOutputStream.write(headerThirdLineByte);
 
         // receiving the response from the aggregation server
-        InputStreamReader in = new InputStreamReader(server.getInputStream());
-        BufferedReader receiver = new BufferedReader(in);
-        String str = "";
-        while ((str = receiver.readLine()) != null) {
-            System.out.println(str);
+        DataInputStream dataInputStream = new DataInputStream(server.getInputStream());
+        int responseHeaderFirstLineLength = dataInputStream.readInt();
+        byte[] responseHeaderFirstLineByte = new byte[responseHeaderFirstLineLength];
+        dataInputStream.readFully(responseHeaderFirstLineByte, 0, responseHeaderFirstLineLength);
+        String responseHeaderFirstLine = new String(responseHeaderFirstLineByte);
+        System.out.println(responseHeaderFirstLine);
+
+        int responseXMLLength = dataInputStream.readInt();
+        byte[] responseXMLByte = new byte[responseXMLLength];
+        dataInputStream.readFully(responseXMLByte, 0, responseXMLLength);
+
+        // write the XML into a temp file
+        File outputFile = new File("GETClientXML/" + uuid.toString() + ".xml");
+        FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+        fileOutputStream.write(responseXMLByte);
+        File tempXMFile = new File("GETClientXML/" + uuid.toString() + ".xml");
+
+        // parse the responded XML file
+        feedQueue = XMLParser.getFeedQueueFromAggregatedXML(tempXMFile);
+        printAggregatedFeed();
+
+        fileOutputStream.close();
+        outputFile.delete();
+    }
+
+    private static void printAggregatedFeed() {
+        for (Feed feed : feedQueue) {
+            System.out.println("Title: " + feed.getTitle());
+            System.out.println("Subtitle: " + feed.getSubtitle());
+            System.out.println("Link: " + feed.getLink());
+            System.out.println("Updated: " + feed.getUpdated());
+            System.out.println("Author: " + feed.getAuthor());
+            System.out.println("Id: " + feed.getId());
+            for (FeedEntry feedEntry : feed.getEntries()) {
+                System.out.println("Entry:");
+                System.out.println("Title: " + feedEntry.getTitle());
+                System.out.println("Link: " + feedEntry.getLink());
+                System.out.println("Id: " + feedEntry.getId());
+                System.out.println("Updated: " + feedEntry.getUpdated());
+                System.out.println("Author: " + feedEntry.getAuthor());
+                System.out.println("Summary: " + feedEntry.getSummary());
+            }
+            System.out.println("====================");
         }
     }
 }
